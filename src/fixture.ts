d123ffce849ab87f0c2ee228814684a6ae6d04ac
bouzuya <m@bouzuya.net>
2018-02-10 12:15:40 +0900

    change fixture signature

--- src/fixture.ts
+++ src/fixture.ts
@@ -1,8 +1,3 @@
-export type SetUpFn<T> = (() => void) | (() => T | Promise<T>);
-export type TearDownFn<T> = ((context?: T) => void | Promise<void>);
-export type TestFn<T, U> = ((context?: T) => void) | ((context?: T) => U | Promise<U>);
-export type Fn<T, U> = (context?: T) => Promise<U | undefined>;
-
 const promiseFinally = <T, U>(promise: Promise<T>, f: () => U): Promise<T> => {
   return promise.then(
     (v) => promiseTry(f).then((_) => Promise.resolve(v), (_) => Promise.resolve(v)),
@@ -14,32 +9,19 @@ const promiseTry = <T>(f: () => T | Promise<T>): Promise<T> => {
   return Promise.resolve().then(() => f());
 };
 
-const wrapFn = <T, U>(f: TestFn<T, U>): Fn<T, U> => {
-  return (context: T | undefined): Promise<U | undefined> => {
-    return promiseTry((): U | Promise<U | undefined> | undefined => {
-      const r: void | U | Promise<U> = f(context);
-      return typeof r === 'undefined' ? void 0 : r;
-    });
-  };
-};
-
 const fixture = <T, U>(
-  options: {
-    setUp?: SetUpFn<T>;
-    tearDown?: TearDownFn<T>;
-  },
-  test: TestFn<T, U>
+  setUp: () => T | Promise<T>,
+  tearDown: (context: T) => void | Promise<void>,
+  test: (context: T) => U | Promise<U>
 ): () => Promise<void> => {
-  const setUpFn: Fn<undefined, T> = wrapFn<undefined, T>(
-    typeof options.setUp !== 'undefined' ? options.setUp : () => void 0
-  );
-  const tearDownFn: Fn<T, undefined> = wrapFn<T, undefined>(
-    typeof options.tearDown !== 'undefined' ? options.tearDown : () => void 0
-  );
-  const testFn: Fn<T, U> = wrapFn<T, U>(test);
   return () => {
-    return setUpFn()
-      .then((c) => promiseFinally(testFn(c), () => tearDownFn(c)))
+    return promiseTry(setUp)
+      .then((c) =>
+        promiseFinally(
+          promiseTry(() => test(c)),
+          () => promiseTry(() => tearDown(c))
+        )
+      )
       .then(() => Promise.resolve());
   };
 };
--- test/fixture.ts
+++ test/fixture.ts
@@ -5,88 +5,80 @@ import { fixture } from '../src/fixture';
 
 const category = '/fixture ';
 const tests: Test[] = [
-  test(category + 'test (void)', () => {
-    const testFn = sinon.stub();
-    return fixture({
-      // no options
-    }, testFn as (() => void))().then(() => {
-      assert(testFn.callCount === 1);
-    });
-  }),
-  test(category + 'setUp (void) -> test (void)', () => {
+  test(category + 'setUp (void) -> test (void) -> tearDown (void)', () => {
     const setUpFn = sinon.stub();
     const testFn = sinon.stub();
-    return fixture({
-      setUp: setUpFn as (() => void)
-    }, testFn as (() => void))().then(() => {
+    const tearDownFn = sinon.stub();
+    return fixture(
+      setUpFn as (() => void),
+      tearDownFn as (() => void),
+      testFn as (() => void)
+    )().then(() => {
+      assert(setUpFn.callCount === 1);
       assert(testFn.callCount === 1);
+      assert(testFn.getCall(0).args.length === 1);
+      assert(testFn.getCall(0).args[0] === undefined);
+      assert(tearDownFn.callCount === 1);
+      assert(tearDownFn.getCall(0).args.length === 1);
+      assert(tearDownFn.getCall(0).args[0] === undefined);
     });
   }),
-  test(category + 'setUp (t) -> test (void)', () => {
+  test(category + 'setUp (t) -> test (void) -> tearDown (void)', () => {
     const context = true;
     const setUpFn = sinon.stub().returns(context);
     const testFn = sinon.stub();
-    return fixture({
-      setUp: setUpFn as (() => typeof context)
-    }, testFn as (() => void))().then(() => {
+    const tearDownFn = sinon.stub();
+    return fixture(
+      setUpFn as (() => typeof context),
+      tearDownFn as (() => void),
+      testFn as (() => void)
+    )().then(() => {
       assert(setUpFn.callCount === 1);
       assert(testFn.callCount === 1);
       assert(testFn.getCall(0).args.length === 1);
       assert(testFn.getCall(0).args[0] === context);
+      assert(tearDownFn.callCount === 1);
+      assert(tearDownFn.getCall(0).args.length === 1);
+      assert(tearDownFn.getCall(0).args[0] === context);
     });
   }),
-  test(category + 'setUp (promise<t>) -> test (void)', () => {
+  test(category + 'setUp (promise<t>) -> test (void) -> tearDown (void)', () => {
     const context = true;
     const setUpFn = sinon.stub().returns(Promise.resolve(context));
     const testFn = sinon.stub();
-    return fixture({
-      setUp: setUpFn as (() => Promise<typeof context>)
-    }, testFn as (() => void))().then(() => {
+    const tearDownFn = sinon.stub();
+    return fixture(
+      setUpFn as (() => Promise<typeof context>),
+      tearDownFn as (() => void),
+      testFn as (() => void)
+    )().then(() => {
       assert(setUpFn.callCount === 1);
       assert(testFn.callCount === 1);
       assert(testFn.getCall(0).args.length === 1);
       assert(testFn.getCall(0).args[0] === context);
+      assert(tearDownFn.callCount === 1);
+      assert(tearDownFn.getCall(0).args.length === 1);
+      assert(tearDownFn.getCall(0).args[0] === context);
     });
   }),
-  test(category + 'setUp (promise<t>) -> test (void)', () => {
-    const context = true;
-    const setUpFn = sinon.stub().returns(Promise.resolve(context));
-    const testFn = sinon.stub();
-    return fixture({
-      setUp: setUpFn as (() => Promise<typeof context>)
-    }, testFn as (() => void))().then(() => {
-      assert(setUpFn.callCount === 1);
-      assert(testFn.callCount === 1);
-      assert(testFn.getCall(0).args.length === 1);
-      assert(testFn.getCall(0).args[0] === context);
-    });
-  }),
-  test(category + 'setUp (promise<t>) -> test (t)', () => {
+  test(category + 'setUp (promise<t>) -> test (t) -> tearDown (void)', () => {
     const context = true;
     const setUpFn = sinon.stub().returns(Promise.resolve(context));
     const value = 123;
     const testFn = sinon.stub().returns(value);
-    return fixture({
-      setUp: setUpFn as (() => Promise<typeof context>)
-    }, testFn as (() => typeof value))().then(() => {
+    const tearDownFn = sinon.stub();
+    return fixture(
+      setUpFn as (() => Promise<typeof context>),
+      tearDownFn as (() => void),
+      testFn as (() => void)
+    )().then(() => {
       assert(setUpFn.callCount === 1);
       assert(testFn.callCount === 1);
       assert(testFn.getCall(0).args.length === 1);
       assert(testFn.getCall(0).args[0] === context);
-    });
-  }),
-  test(category + 'setUp (promise<t>) -> test (promise<t>)', () => {
-    const context = true;
-    const setUpFn = sinon.stub().returns(Promise.resolve(context));
-    const value = 123;
-    const testFn = sinon.stub().returns(Promise.resolve(value));
-    return fixture({
-      setUp: setUpFn as (() => Promise<typeof context>)
-    }, testFn as (() => Promise<typeof value>))().then(() => {
-      assert(setUpFn.callCount === 1);
-      assert(testFn.callCount === 1);
-      assert(testFn.getCall(0).args.length === 1);
-      assert(testFn.getCall(0).args[0] === context);
+      assert(tearDownFn.callCount === 1);
+      assert(tearDownFn.getCall(0).args.length === 1);
+      assert(tearDownFn.getCall(0).args[0] === context);
     });
   }),
   test(category + 'setUp (promise<t>) -> test (promise<t>) -> tearDown (void)', () => {
@@ -95,10 +87,11 @@ const tests: Test[] = [
     const value = 123;
     const testFn = sinon.stub().returns(Promise.resolve(value));
     const tearDownFn = sinon.stub();
-    return fixture({
-      setUp: setUpFn as (() => Promise<typeof context>),
-      tearDown: tearDownFn as (() => void)
-    }, testFn as (() => Promise<typeof value>))().then(() => {
+    return fixture(
+      setUpFn as (() => Promise<typeof context>),
+      tearDownFn as (() => void),
+      testFn as (() => void)
+    )().then(() => {
       assert(setUpFn.callCount === 1);
       assert(testFn.callCount === 1);
       assert(testFn.getCall(0).args.length === 1);
@@ -114,10 +107,11 @@ const tests: Test[] = [
     const value = 123;
     const testFn = sinon.stub().returns(Promise.resolve(value));
     const tearDownFn = sinon.stub().returns(Promise.resolve());
-    return fixture({
-      setUp: setUpFn as (() => Promise<typeof context>),
-      tearDown: tearDownFn as (() => Promise<void>)
-    }, testFn as (() => Promise<typeof value>))().then(() => {
+    return fixture(
+      setUpFn as (() => Promise<typeof context>),
+      tearDownFn as (() => Promise<void>),
+      testFn as (() => Promise<typeof value>)
+    )().then(() => {
       assert(setUpFn.callCount === 1);
       assert(testFn.callCount === 1);
       assert(testFn.getCall(0).args.length === 1);
